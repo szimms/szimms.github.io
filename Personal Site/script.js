fa170d62bda8c10d43ac067e4e0fee9105ab0502
Solomon Zimmermann <78663212+szimms@users.noreply.github.com>
2025-06-19 08:58:09 -0500

    inital commit

---
+++ Personal Site/script.js
@@ -0,0 +1,36 @@
+   const carouselContainer = document.querySelector('.carousel-container');
+   const carouselItems = document.querySelector('.carousel');
+   const prevButton = document.querySelector('.left-arrow');
+   const nextButton = document.querySelector('.right-arrow');
+   const items = Array.from(document.querySelectorAll('.item'));
+   let currentItem = 0;
+   
+   
+   
+   function showItem(index){
+       items.forEach(item => item.style.display = 'none');
+       items[index].style.display = 'flex';
+   }
+
+   showItem(currentItem);
+   
+   prevButton.addEventListener('click', () => {
+       currentItem = (currentItem - 1 + items.length) % items.length;
+       showItem(currentItem);
+   });
+   
+   nextButton.addEventListener('click', () => {
+       currentItem = (currentItem + 1) % items.length;
+       showItem(currentItem);
+   });
+
+
+
+    function myFunction() {
+        var x = document.getElementById("menuUL");
+        if (x.style.display === "block") {
+            x.style.display = "none";
+        } else {
+            x.style.display = "block";
+        }
+}
